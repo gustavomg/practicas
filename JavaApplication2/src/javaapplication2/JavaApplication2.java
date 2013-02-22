e76ca16bb0b16f46b585f79d1bd791de3b26c643
gustavo.millan <gustavo.millan@hp3031-21.CENTRO.LOCAL>
2013-02-22 21:32:12 +0100

    obtener el entorno de nuevo

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,6 +14,6 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //System.getenv();
+       System.getenv();
     }
 }
